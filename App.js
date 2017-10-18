f66333848874cbc3e736819fcf79bfc6e8483491
liuzhihui <liuzhihui@postop.cn>
2017-10-18 16:29:16 +0800

    Fix。结构调整。

--- App.js
+++ App.js
@@ -5,129 +5,18 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View, Image
-} from 'react-native';
 
-import {StackNavigator, TabNavigator} from 'react-navigation';
+import {StackNavigator} from 'react-navigation';
 
-//redux
-import {Provider} from 'react-redux'
-
-//scene
-import HomeScreen from './js/screens/HomeScreen'
-import DetailScreen from './js/screens/DetailScreen'
-import CartScreen from './js/screens/CartScreen'
-import MeScreen from './js/screens/MeScreen'
 import LoginScreen from './js/screens/LoginScreen'
-import Icon from 'react-native-vector-icons/Ionicons'
-
-//util
-import Colors from './js/utils/color'
-
-
-//测试StackNavigator
-const stackNavigatorApp = StackNavigator({
-    Home   : {screen: HomeScreen},
-    Detail : {screen: DetailScreen}
-});
-
-// export default stackNavigatorApp;
-
-//测试TabNavigator
-const tabBarNavigatorApp = TabNavigator({
-    Home : {
-        screen : HomeScreen,
-        navigationOptions : {
-            tabBarLabel : '生鲜货架',
-            tabBarIcon: (({tintColor,focused}) => {
-                return(
-                    <Icon name="ios-home" size={30} color={tintColor} />
-                )
-            }), // 设置标签栏的图标。需要单独设置。
-
-        }
-
-    },
-    Cart : {
-        screen : CartScreen,
-        navigationOptions : {
-            tabBarLabel : '购物车',
-            tabBarIcon: (({tintColor,focused}) => {
-                return(
-                <Icon name="ios-cart" size={30} color={tintColor} style={{marginLeft:-5}} />
-                )
-            }), // 设置标签栏的图标。需要单独设置。
-
-        }
-    },
-    Me : {
-        screen : MeScreen,
-        navigationOptions : {
-            tabBarLabel : '我',
-            tabBarIcon: (({tintColor,focused}) => {
-                return(
-                    <Icon name="ios-person" size={30} color={tintColor} />
-                )
-            }), // 设置标签栏的图标。需要单独设置。
-
-        }
-    },
-    },
-
-    {
-        tabBarPosition : 'bottom',
-        swipeEnabled : Platform.OS == 'ios' ? false : true,
-        tabBarOptions: {
-            activeTintColor: Colors.MainColor, // 文字和图片选中颜色
-            inactiveTintColor: '#999', // 文字和图片未选中颜色
-            showIcon: true, // android 默认不显示 icon, 需要设置为 true 才会显示
-            indicatorStyle: {
-                height: 0  // 如TabBar下面显示有一条线，可以设高度为0后隐藏
-            },
-            style: {
-                backgroundColor: '#fff', // TabBar 背景色
-                height: Platform.OS == 'ios' ? 54 : 54,
-            },
-            labelStyle: {
-                fontSize: 10, // 文字大小
-
-            },
-            iconStyle : {
-
-            }
-        },
-
-    }
-);
+import TabScreen from './js/screens/TabScreen'
 
 
 
-
-// export default tabBarNavigatorApp;
-
-//将StackNavigator与TabNavigator结合起来
-const composeTabAndStackNavigatorApp = StackNavigator({
-    Tab : {
-        screen : tabBarNavigatorApp,
-        navigationOptions : {
-            headerTintColor : Colors.MainColor,
-        }
-    },
-    Detail : {
-        screen : DetailScreen,
-        navigationOptions : {
-            headerTintColor : Colors.MainColor,
-        }
-    }
-});
-
-export default composeTabAndStackNavigatorApp;
+export default TabScreen;
 
 //加入login的逻辑。
+
 const freshToHomeApp = StackNavigator({
     Login : {
         screen : LoginScreen,
@@ -136,65 +25,14 @@ const freshToHomeApp = StackNavigator({
         }
     },
     Main : {
-        screen : composeTabAndStackNavigatorApp,
+        screen : TabScreen,
         navigationOptions : {
             header : null,
         }
     }
 });
+
 // export default freshToHomeApp;
 
 
 
-
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-// export default class App extends Component<{}> {
-//   // render() {
-//   //   return (
-//   //     <View style={styles.container}>
-//   //       <Text style={styles.welcome}>
-//   //         Welcome to React Native!
-//   //       </Text>
-//   //       <Text style={styles.instructions}>
-//   //         To get started, edit App.js
-//   //       </Text>
-//   //       <Text style={styles.instructions}>
-//   //         {instructions}
-//   //       </Text>
-//   //     </View>
-//   //   );
-//   // }
-//
-//     render(){
-//       return(
-//           <SimpleApp/>
-//       );
-//     }
-//
-//
-// }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- js/screens/TabScreen/TabScreen.js
+++ js/screens/TabScreen/TabScreen.js
@@ -1,34 +1,143 @@
 /**
  * Created by liuzhihui on 2017/10/10.
  */
-import React,{Component} from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React from 'react';
+import {
+    Platform,
+} from 'react-native';
+
+import {StackNavigator, TabNavigator} from 'react-navigation';
+
+//scene
+import HomeScreen from '../HomeScreen'
+import DetailScreen from '../DetailScreen'
+import CartScreen from '../CartScreen'
+import MeScreen from '../MeScreen'
+
 import Icon from 'react-native-vector-icons/Ionicons'
 
+//util
+import Colors from '../../utils/color'
 
-export default class TabScreen extends Component{
 
-    render(){
-        return(
-            <Text style={styles.text}>TabScreen</Text>
-    );
+
+function _swipeEnable() {
+
+    if(Platform.OS === 'ios')
+    {
+        return true;
     }
+
+    return true;
 
 }
 
-const styles = StyleSheet.create(
+function _tabbarHeight() {
+
+    if(Platform.OS === 'ios')
     {
-        container1 : {
-            flex : 1,
-            backgroundColor : '#e50263'
+        return 54;
+    }
+
+    return 54;
+
+}
+
+
+//测试StackNavigator
+const stackNavigatorApp = StackNavigator({
+    Home   : {screen: HomeScreen},
+    Detail : {screen: DetailScreen}
+});
+
+// export default stackNavigatorApp;
+
+//测试TabNavigator
+const tabBarNavigatorApp = TabNavigator({
+        Home : {
+            screen : HomeScreen,
+            navigationOptions : {
+                tabBarLabel : '生鲜货架',
+                tabBarIcon: (({tintColor,focused}) => {
+                    return(
+                        <Icon name="ios-home" size={30} color={tintColor} />
+                    )
+                }), // 设置标签栏的图标。需要单独设置。
+
+            }
+
         },
-        text : {
-            color : '#e50263'
-        }
+        Cart : {
+            screen : CartScreen,
+            navigationOptions : {
+                tabBarLabel : '购物车',
+                tabBarIcon: (({tintColor,focused}) => {
+                    return(
+                        <Icon name="ios-cart" size={30} color={tintColor} style={{marginLeft:-5}} />
+                    )
+                }), // 设置标签栏的图标。需要单独设置。
+
+            }
+        },
+        Me : {
+            screen : MeScreen,
+            navigationOptions : {
+                tabBarLabel : '我',
+                tabBarIcon: (({tintColor,focused}) => {
+                    return(
+                        <Icon name="ios-person" size={30} color={tintColor} />
+                    )
+                }), // 设置标签栏的图标。需要单独设置。
+
+            }
+        },
+    },
+
+    {
+        tabBarPosition : 'bottom',
+        swipeEnabled : _swipeEnable(),
+        tabBarOptions: {
+            activeTintColor: Colors.MainColor, // 文字和图片选中颜色
+            inactiveTintColor: '#999', // 文字和图片未选中颜色
+            showIcon: true, // android 默认不显示 icon, 需要设置为 true 才会显示
+            indicatorStyle: {
+                height: 0  // 如TabBar下面显示有一条线，可以设高度为0后隐藏
+            },
+            style: {
+                backgroundColor: '#fff', // TabBar 背景色
+                height: _tabbarHeight(),
+            },
+            labelStyle: {
+                fontSize: 10, // 文字大小
+
+            },
+            iconStyle : {
+
+            }
+        },
+
     }
 );
 
-const HomeIcon = ()=>{
-    return <Icon name="ios-home" size={30} color="red"></Icon>
-};
 
+
+
+// export default tabBarNavigatorApp;
+
+//将StackNavigator与TabNavigator结合起来
+const composeTabAndStackNavigatorApp = StackNavigator({
+    Tab : {
+        screen : tabBarNavigatorApp,
+        navigationOptions : {
+            headerTintColor : Colors.MainColor,
+        }
+    },
+    Detail : {
+        screen : DetailScreen,
+        navigationOptions : {
+            headerTintColor : Colors.MainColor,
+        }
+    }
+});
+
+export default composeTabAndStackNavigatorApp;
