e7638d8c42383c5b00d7fe5f709309619fc46993
liuzhihui <liuzhihui@postop.cn>
2017-10-19 10:13:25 +0800

    ADD。scrollable tab view。

--- js/screens/CartScreen/CartScreen.js
+++ js/screens/CartScreen/CartScreen.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'
 import {View,
     Text,
-    StyleSheet
 } from 'react-native'
 
-import CommonStyles from '../../utils/css/styles'
+import ScrollableTabView, {DefaultTabBar,} from 'react-native-scrollable-tab-view'
+
 
 export default class CartScreen extends Component{
 
@@ -14,9 +14,16 @@ export default class CartScreen extends Component{
 
     render(){
         return(
-        <View style={CommonStyles.center_layout_container}>
-            <Text>Cart Screen</Text>
-        </View>
+
+            <ScrollableTabView
+                style={{marginTop: 20, }}
+                initialPage={1}
+                renderTabBar={() => <DefaultTabBar />}
+                >
+                <Text tabLabel='Tab #1'>My</Text>
+                <Text tabLabel='Tab #2'>favorite</Text>
+                <Text tabLabel='Tab #3'>project</Text>
+            </ScrollableTabView>
         );
     };
 }
--- js/utils/persistance/storage/index.js
+++ js/utils/persistance/storage/index.js
@@ -2,6 +2,8 @@
  * Created by liuzhihui on 2017/10/19.
  */
 
+//https://github.com/sunnylqm/react-native-storage
+
 import storage from './storage'
 
 export default storage;
