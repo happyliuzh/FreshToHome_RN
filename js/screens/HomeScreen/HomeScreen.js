5ba8ab48d058adb254bc03f208e5d05af8c80a4d
liuzhihui <liuzhihui@postop.cn>
2017-10-23 21:41:06 +0800

    Fix。homescreen loading

--- js/screens/HomeScreen/HomeScreen.js
+++ js/screens/HomeScreen/HomeScreen.js
@@ -13,17 +13,9 @@ import Colors from '../../utils/color'
 import CarouselView from "../../components/Home/carouseView";
 import Images from '../../utils/image'
 import HomeView from '../../components/Home/homeView'
+import Loader from '../../utils/loader'
 
-// <Image style={homeStyles.image} source={Images.TabIcon.Run_Image_F}></Image>
-// <Image style={homeStyles.image} source={Images.TabIcon.Run_Image_S}></Image>
-// <Image style={homeStyles.image} source={Images.TabIcon.Me_Image_F}></Image>
-// <Image style={homeStyles.image} source={Images.TabIcon.Me_Image_S}></Image>
-//
-// <Image style={[homeStyles.image,homeStyles.wideImage]} source={Images.WebImage.BaiduLogo_Image}></Image>
-// <Image style={[homeStyles.image,homeStyles.wideImage,homeStyles.largeImage]} source={Images.WebImage.Http_Image}></Image>
-//
-//
-// <Button title="点我进入详情-----》" onPress= {() => navigate('Detail',{params : "hello are you detail screen?"})}/>
+
 
 
 
@@ -35,18 +27,40 @@ export default class HomeScreen extends Component{
         headerBackTitle : '返回',
     };
 
+    constructor(){
+        super();
+
+        this.state = {
+           isLoaded : false,
+        };
+    }
+
     componentWillMount(){
 
         console.log('Home Screen will mount');
+
     }
 
     componentDidMount(){
 
         console.log('Home Screen did mount');
+
+        setTimeout(()=>{
+
+            this.setState({
+                isLoaded:true,
+            });
+
+        }, 2000);
     }
 
     render(){
         const { navigate } = this.props.navigation;
+
+        if(!this.state.isLoaded)
+        {
+            return <Loader/>
+        }
 
         return (
             <HomeView>
