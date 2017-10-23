27beb8915974c9233bbf5a128adaaa4a2c338580
liuzhihui <liuzhihui@postop.cn>
2017-10-23 09:49:38 +0800

    sectionList增加刷新逻辑。

--- js/components/Home/homeView/homeView.js
+++ js/components/Home/homeView/homeView.js
@@ -115,6 +115,10 @@ export default class HomeView extends Component
             { key: "B", data: [{ title: "表哥" }, { title: "贝贝" }, { title: "表弟" }, { title: "表姐" }, { title: "表叔" }] },
             { key: "C", data: [{ title: "成吉思汗" }, { title: "超市快递" }] },
             { key: "W", data: [{ title: "王磊" }, { title: "王者荣耀" }, { title: "往事不能回味" },{ title: "王小磊" }, { title: "王中磊" }, { title: "王大磊" }] },
+            { key: "Q", data: [{ title: "王磊" }, { title: "王者荣耀" }, { title: "往事不能回味" },{ title: "王小磊" }, { title: "王中磊" }, { title: "王大磊" }] },
+            { key: "Z", data: [{ title: "王磊" }, { title: "王者荣耀" }, { title: "往事不能回味" },{ title: "王小磊" }, { title: "王中磊" }, { title: "王大磊" }] },
+            { key: "T", data: [{ title: "王磊" }, { title: "王者荣耀" }, { title: "往事不能回味" },{ title: "王小磊" }, { title: "王中磊" }, { title: "王大磊" }] },
+
         ];
 
         return (
@@ -125,6 +129,10 @@ export default class HomeView extends Component
                              renderSectionHeader = {this._renderHeader}
                              keyExtractor={(item, index) => {return index + '';}}  //this is required when you are using FlatList
                              ListHeaderComponent={this._listHeader}
+                             onRefresh = {()=>{
+
+                             }}
+                             refreshing={false}
                 >
 
                 </SectionList>
@@ -136,7 +144,6 @@ export default class HomeView extends Component
 const homeViewStyles = StyleSheet.create({
     container : {
         flex : 1,
-        backgroundColor : Colors.MainColor,
     },
 
 });
